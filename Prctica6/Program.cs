d447174b9e6c2713865868b8145040f187fa2a3f
Mary <is24maskinama@artcollege.ru>
2024-03-04 04:30:15 +0300

    Написание кода 5Cos(2x).

--- Prctica6/Program.cs
+++ Prctica6/Program.cs
@@ -10,6 +10,14 @@ namespace Prctica6
     {
         static void Main(string[] args)
         {
+            Console.Write("Write the value of x= ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double f = 5 * Math.Cos(2 * x);
+            Console.WriteLine($"F is value = {f}");
+            Console.WriteLine("Maskina");
+            Console.ReadLine();
+
+            
 
         }
     }
